88732868c5be532afeeace27952aecaae6ab7a96
Paula <paularffr@gmail.com>
2021-12-10 02:16:26 -0300

    Initial commit

---
+++ src/resolucaodatela/ResolucaoDaTela.java
@@ -0,0 +1,12 @@
+package resolucaodatela;
+
+
+import java.awt.*;
+
+public class ResolucaoDaTela {
+    public static void main(String[] args) {
+        Toolkit tk = Toolkit.getDefaultToolkit();
+        Dimension d = tk.getScreenSize();
+        System.out.println("A resolução do seu sistema é " + d.width + "X" + d.height);
+    }
+}
